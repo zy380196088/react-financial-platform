29722d172eb56c494a3c33f6f9142da2f431bc05
Joy <380196088@qq.com>
2020-11-06 17:08:05 +0800

    s

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 // import ReactDom from 'react-dom';
 // import { Provider } from 'react-redux';
 import Home from './components/Home'
-import {Button} from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button type="primary">START React</Button>
           <Home/>
       </header>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,56 @@
-import React , { Component } from 'react';
-class Home extends Component{
-    render(){
-        return <div>Home</div>
-    }
+import React, {
+  Component
+} from 'react';
+import {
+  Layout,
+  Menu
+} from 'antd';
+
+const {
+  Header,
+  Sider,
+  Content
+} = Layout;
+
+class Home extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <Sider trigger={null} collapsed={this.state.collapsed}>
+          <div className="logo"/>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              Nav 1
+            </Menu.Item>
+            <Menu.Item key="2">
+              Nav 2
+            </Menu.Item>
+            <Menu.Item key="3">
+              Nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header></Header>
+          <Content style={{
+            margin: '24px 16px',
+            padding:24,
+            minHeight:280
+          }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    )
+  }
 }
 
 export default Home;
